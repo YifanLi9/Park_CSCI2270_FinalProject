53acd32db04ae50f144653173400b26a29d6b733
YIFAN LI <yili4303@colorado.edu>
2016-05-03 12:54:19 -0600

    Update ItemTree.h

--- ItemTree.h
+++ ItemTree.h
@@ -2,6 +2,10 @@
 // CSCI 2270 Hoegniman
 // Final Project
 
+// Seungjeong Park
+// CSCI 2270 Hoegniman
+// Final Project
+
 #ifndef ITEMTREE_H
 #define ITEMTREE_H
 #include <string>
@@ -14,9 +18,9 @@ struct ItemNode
     ItemNode *parent;
     ItemNode *leftChild;
     ItemNode *rightChild;
-
+    
     ItemNode(){};
-
+    
     ItemNode(std::string in_item, int in_price)
     {
         item = in_item;
@@ -26,32 +30,33 @@ struct ItemNode
 
 class ItemTree
 {
-    public:
-        ItemTree();
-        virtual ~ItemTree();
-        void addItemNode(std::string item, int price);
-        void deleteItemNode(std::string item);
-        void findItem(std::string item);
-        int countItemNodes();
-        void printInOrder();
-        void printPreOrder();
-        void printPostOrder();
-        void printSold();
-
-    protected:
-
-    private:
-        ItemNode* minimumNode(ItemNode *node);
-        ItemNode* searchItemTree(ItemNode *node, std::string item);
-        int countItemNodes(ItemNode *node);
-        void printInOrder(ItemNode *node);
-        void printPreOrder(ItemNode *node);
-        void printPostOrder(ItemNode *node);
-        void DeleteAll(ItemNode *node); //use this for the post-order traversal deletion of the tree
-
-        ItemNode *root;
-        int cnt;
-        std::vector <std::string> soldList;
+public:
+    ItemTree();
+    virtual ~ItemTree();
+    void addItemNode(std::string item, int price);
+    void deleteItemNode(std::string item);
+    void findItem(std::string item);
+    int countItemNodes();
+    void printInOrder();
+    void printPreOrder();
+    void printPostOrder();
+    void printSold();
+    void meun();
+    
+protected:
+    
+private:
+    ItemNode* minimumNode(ItemNode *node);
+    ItemNode* searchItemTree(ItemNode *node, std::string item);
+    int countItemNodes(ItemNode *node);
+    void printInOrder(ItemNode *node);
+    void printPreOrder(ItemNode *node);
+    void printPostOrder(ItemNode *node);
+    void DeleteAll(ItemNode *node); //use this for the post-order traversal deletion of the tree
+    
+    ItemNode *root;
+    int cnt;
+    std::vector <std::string> soldList;
 };
 
 #endif // MOVIETREE_H
